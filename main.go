f56505820d81342331e27f739000a5e4996f4487
Jacques Nguyen <jacques@cadeaudelamaison.com>
2024-08-05 22:59:53 +0200

    setup template

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "log"
     "github.com/gofiber/fiber/v2"
     "github.com/gofiber/fiber/v2/middleware/cors"
+    "github.com/gofiber/template/html/v2"
 )
 
 import "my-tenant-backend-v2/tenant"
@@ -13,15 +14,25 @@ func status(c *fiber.Ctx) error{
 	return c.SendString("Server is up and running !")
 }
 func setupRoutes(app *fiber.App) {
-	app.Get("/", status)
+	app.Get("/", func(c *fiber.Ctx) error {
+        // Render index template
+        return c.Render("index", fiber.Map{
+            "Title": "Hello, World!",
+        })
+    })
 	app.Get("api/tenants", tenant.GetAllTenants)
 	app.Post("api/tenant", tenant.InsertTenant)
 }
 
 func main(){
     // Init
-    app := fiber.New()
+
     dbErr := db.InitDB()
+    engine := html.New("./views", ".html")
+
+    app := fiber.New(fiber.Config{
+            Views: engine,
+    })
 
     app.Use(cors.New(cors.Config{
         AllowOrigins: "*",
